fdbbc39e958224c5a066f64038919da0a56537ae
Elliot Briant <briantmoliveira@gmail.com>
2018-04-28 13:13:58 -0700

    setting up backend server

---
+++ app.js
@@ -0,0 +1,31 @@
+/********************************
+* Marconi-Clone main Server
+********************************/
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv')
+
+
+// Initializing server
+const app = express();
+
+// Defining the PORT number
+const PORT = process.env.PORT || 3000
+
+
+// Allow Cors
+
+/******************************
+*   Middleware
+******************************/
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(allowCrossDomain);
+
+// Load Routes
+
+// Initializing listener
+app.listen(PORT, function() {
+  console.log('Marconi Clone listening on port ', PORT);
+});
